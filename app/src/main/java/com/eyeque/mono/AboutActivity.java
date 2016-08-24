3bbc7b2b3017c796886cc817d77ac5b1375acd36
gzateyeque <george.zhao@eyeque.com>
2016-08-23 19:01:23 -0700

    Added copyright

--- app/src/main/java/com/eyeque/mono/AboutActivity.java
+++ app/src/main/java/com/eyeque/mono/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.eyeque.mono;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView rateAppTv = (TextView) findViewById(R.id.rateAppTextStringView);
+        rateAppTv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri =  Uri.parse("http://www.eyeque.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        TextView websiteTv = (TextView) findViewById(R.id.websiteTextStringView);
+        websiteTv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri =  Uri.parse("http://www.eyeque.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
