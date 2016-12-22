37cd3d1502b6148c42807e192ba6144f90676d03
gzateyeque <george.zhao@eyeque.com>
2016-12-22 14:52:02 -0800

    version 1.1.0

--- app/src/main/java/com/eyeque/mono/SingletonDataHolder.java
+++ app/src/main/java/com/eyeque/mono/SingletonDataHolder.java
@@ -1,6 +1,7 @@
 package com.eyeque.mono;
 
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by georgez on 6/16/16.
@@ -111,116 +112,10 @@ public class SingletonDataHolder {
     public static int greenDegree = 0;
     public static int blueDegree = 0;
     // Calculation configuration
-    public static double sphericalStep0 = 1.428405590E-01*577/phonePpi;
-    public static double sphericalStep1 = 1.276384330E-01*577/phonePpi;
-    public static double sphericalStep2 = 6.454937531E-05*(577/phonePpi)*(577/phonePpi);
-
-    /*****
-     // Phone profile for Galaxy 6
-     public static String phoneBrand = "Samsung";      // Need to add it to database
-     public static String phoneModel = "SM-G930F";     // Need to add it to database
-     public static String phoneType = "Galaxy 6";
-     public static int phonePpi = 577;
-     // Device configuration
-     public static String deviceName = "EQ100";
-     public static double DEVICE_WIDTH = 2.0f;
-     public static double DEVICE_HEIGHT = 1.375f;
-     // Pattern drawing configuration
-     public static int CENTER_X = 720;
-     public static int CENTER_Y = 520;
-     public static int LINE_LENGTH = 130;
-     public static int LINE_WIDTH = 36;
-     public static int INIT_DISTANCE = 326;
-     public static int MIN_DISTANCE = 227;
-     public static int MAX_DISTANCE = 410;
-     /*** For Video Shooting App
-     public static int INIT_DISTANCE = 99;
-     public static int MIN_DISTANCE = 0;
-     public static int MAX_DISTANCE = 183;
-     public static final int[] PATTERN_ANGLE_LIST  = {0, 320, 280, 240, 200, 160, 120, 80, 40};
-     public static final double[] CALC_ANGLE_LIST = {0.0, 320.0, 280.0, 240.0, 200.0, 160.0, 120.0, 80.0, 40.0};
-     public static final int[] ROTATE_ANGLE_LIST = {180, 40, 80, 120, 160, 200, 240, 280, 320};
-     // Color configuration
-     public static int RED_DEGREE = 0;
-     public static int GREEN_DEGREE = 0;
-     public static int BLUE_DEGREE = 0;
-     // Calculation configuration
-     public static double SphericalStep0 = 1.428405590E-01*577/PHONE_PPI;
-     public static double SphericalStep1 = 1.276384330E-01*577/PHONE_PPI;
-     public static double SphericalStep2 = 6.454937531E-05*(577/PHONE_PPI)*(577/PHONE_PPI);
-
-     // Phone profile for Galaxy 5
-     public static String PHONE_BRAND = "Samsung";      // Need to add it to database
-     public static String PHONE_MODEL = "SAMSUNG-SM-G900A";     // Need to add it to database
-     public static String PHONE_TYPE = "Galaxy 5";
-     public static int PHONE_PPI = 432;
-     // Device configuration
-     public static String DEVICE_NAME = "EQ100";
-     public static double DEVICE_WIDTH = 2.0f;
-     public static double DEVICE_HEIGHT = 1.375f;
-     // Pattern drawing configuration
-     public static int CENTER_X = 540;
-     public static int CENTER_Y = 390;
-     public static int LINE_LENGTH = 98;
-     public static int LINE_WIDTH = 27;
-     public static int INIT_DISTANCE = 326*PHONE_PPI/577;
-     public static int MIN_DISTANCE = 227*PHONE_PPI/577;
-     public static int MAX_DISTANCE = 410*PHONE_PPI/577;
-
-     // Phone profile for Nexus 6P
-     public static String PHONE_BRAND = "Huawei";      // Need to add it to database
-     public static String PHONE_MODEL = "Nexus 6P";     // Need to add it to database
-     public static String PHONE_TYPE = "Nexus 6P";
-     public static int PHONE_PPI = 518;
-     // Device configuration
-     public static String DEVICE_NAME = "Device 5";
-     public static double DEVICE_WIDTH = 2.0f;
-     public static double DEVICE_HEIGHT = 1.375f;
-     // Pattern drawing configuration
-     public static int CENTER_X = 720;
-     public static int CENTER_Y = 500;
-     public static int LINE_LENGTH = 130*PHONE_PPI/562;
-     public static int LINE_WIDTH = 36*PHONE_PPI/562;
-     public static int INIT_DISTANCE = 326*PHONE_PPI/562;
-     public static int MIN_DISTANCE = 227*PHONE_PPI/562;
-     public static int MAX_DISTANCE = 410*PHONE_PPI/562;
-
-     // Phone profile for LG G5
-     public static String PHONE_BRAND = "LGE";
-     public static String PHONE_MODEL = "LG-H860";
-     public static String PHONE_TYPE = "LG G5";
-     public static int PHONE_PPI = 554;
-     // Device configuration
-     public static String DEVICE_NAME = "Device 5";
-     public static double DEVICE_WIDTH = 2.0f;
-     public static double DEVICE_HEIGHT = 1.375f;
-     // Pattern drawing configuration
-     public static int CENTER_X = 720;
-     public static int CENTER_Y = 520;
-     public static int LINE_LENGTH = 130*PHONE_PPI/562;
-     public static int LINE_WIDTH = 36*PHONE_PPI/562;
-     public static int INIT_DISTANCE = 326*PHONE_PPI/562;
-     public static int MIN_DISTANCE = 227*PHONE_PPI/562;
-     public static int MAX_DISTANCE = 410*PHONE_PPI/562;
-
-
-     public static String PHONE_TYPE = "HTC One S9";
-     public static String PHONE_BRAND = "HTC";      // Need to add it to database
-     public static String PHONE_MODEL = "HTC_S9u";     // Need to add it to database
-     public static int PHONE_PPI = 441;
-     // Device configuration
-     public static String DEVICE_NAME = "Device 5";
-     public static double DEVICE_WIDTH = 2.0f;
-     public static double DEVICE_HEIGHT = 1.375f;
-     // Pattern drawing configuration
-     public static int CENTER_X = 540;
-     public static int CENTER_Y = 385;
-     public static int LINE_LENGTH = 130*PHONE_PPI/577;
-     public static int LINE_WIDTH = 36*PHONE_PPI/577;
-     public static int INIT_DISTANCE = 326*PHONE_PPI/577+1;
-     public static int MIN_DISTANCE = 227*PHONE_PPI/577;
-     public static int MAX_DISTANCE = 410*PHONE_PPI/577;
-     ****/
-
+    public static List<String> sphericalStep;
+    public static double SphericalStep0 = 0.127068;
+    public static double SphericalStep1 = 0.00039076;
+    public static double SphericalStep2 = 0.13464368;
+    public static double SphericalStep3 = 0.00019929366;
 
 }
